465bb1372ffff1859120c96561b1f16c58cb6eb9
YermaniRo <msmdh@gmail.com>
2025-05-28 12:55:08 -0500

    DML

---
+++ DML.sql
@@ -0,0 +1,26 @@
+INSERT INTO fabricante (nombre) VALUES
+('Asus'),
+('Lenovo'),
+('Hewlett-Packard'),
+('Samsung'),
+('Seagate'),
+('Crucial'),
+('Gigabyte'),
+('Huawei'),
+('Xiaomi');
+
+INSERT INTO producto (nombre, precio, codigo_fabricante) VALUES
+('Disco duro SATA3 1TB', '86.99','5'),
+('Memoria RAM DDR4 8GB', '120','6'),
+('Disco SSD 1 TB', '150.99','4'),
+('GeForce GTX 1050Ti ', '185','7'),
+('GeForce GTX 1080 Xtreme', '755', '6'),
+('Monitor 24 LED Full HD ', '202','1'),
+('Monitor 27 LED Full HD', '245.99', '1'),
+('Portátil Yoga 520', '559','2'),
+('Portátil Ideapd 320', '444','2'),
+('Impresora HP Deskjet 3720', '59.99','3'),
+('Impresora HP Laserjet Pro M26nw ', '180','3');
+
+
+
